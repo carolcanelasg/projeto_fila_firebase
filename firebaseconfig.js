050ad0e80637f513014528ca80d16bd95b7f2250
carolcanelasg <caroolcanelas@gmail.com>
2024-05-01 14:27:40 -0300

    consertar arquivo de config e adicionar autenticação

---
+++ firebaseconfig.js
@@ -0,0 +1,46 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+import {
+  getAuth,
+  signInWithRedirect,
+  browserSessionPersistence,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAnwVECL8SAhShBFT4-nEYjkt4tZ34NiLo",
+  authDomain: "prj-unilasalle-kevin-borba.firebaseapp.com",
+  databaseURL: "https://prj-unilasalle-kevin-borba-default-rtdb.firebaseio.com",
+  projectId: "prj-unilasalle-kevin-borba",
+  storageBucket: "prj-unilasalle-kevin-borba.appspot.com",
+  messagingSenderId: "1006054485314",
+  appId: "1:1006054485314:web:478338e52308a5ac6d2cdb",
+  measurementId: "G-PMCPZ9RGQ1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const provider = new GoogleAuthProvider();
+provider.addScope("https://www.googleapis.com/auth/userinfo.email");
+provider.addScope("https://www.googleapis.com/auth/userinfo.profile");
+//const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+auth.languageCode = "pt-BR";
+auth.setPersistence(browserSessionPersistence);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = "/paginas/init.html";
+  } else {
+    signInWithRedirect(auth, provider);
+  }
+});
